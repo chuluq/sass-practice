c4753db9c8a3eceb9863533f9f55ebe46829d143
chuluq <choirulchuluq10@gmail.com>
2022-05-24 10:32:54 +0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
+
+function buildStyles() {
+  return src("ignite/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
+}
+
+function watchTask() {
+  watch(["ignite/**/*.scss", "*.html"], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
